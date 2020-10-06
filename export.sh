949625fd4f3e64af7bdfcb95698f659a2c2a5fa5
appkr <juwonkim@me.com>
2020-10-06 10:44:40 +0900

    removed comment on HOMEBREW_CASK_OPTS

--- export.sh
+++ export.sh
@@ -9,7 +9,7 @@ export EDITOR=vim
 # @see. https://github.com/caskroom/homebrew-cask/blob/master/USAGE.md#options
 #-------------------------------------------------------------------------------
 
-#export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+export HOMEBREW_CASK_OPTS="--appdir=/Applications --fontdir=/Library/Fonts"
 
 #-------------------------------------------------------------------------------
 # Export MONO Compile Time Variable
